68687560cc6b6b1f20a261a40e3ca247b5a183f3
saxenabhishek <as7122000@gmail.com>
2025-11-06 22:17:49 -0500

    refactor(b): separate validation from call

--- server/internal/ip/ip.go
+++ server/internal/ip/ip.go
@@ -10,15 +10,15 @@ import (
 )
 
 type Response struct {
-	Status	string	`json:"status"`
-	Country	string	`json:"country"`
-	City 	string	`json:"city"`
-	Region 	string	`json:"region"`
-	RegionName 	string	`json:"regionName"`
-	Message	string 	`json:"message"`
+	Status     string `json:"status"`
+	Country    string `json:"country"`
+	City       string `json:"city"`
+	Region     string `json:"region"`
+	RegionName string `json:"regionName"`
+	Message    string `json:"message"`
 }
 
-func construct_url(baseURL string, user_ip string) string{
+func construct_url(baseURL string, user_ip string) string {
 	return baseURL + "/" + user_ip
 }
 
@@ -26,39 +26,48 @@ func isRespValid(code int) bool {
 	return code >= 200 && code <= 299
 }
 
-func getRegion(url string, client *http.Client) (string, error) {
+func makeHttpCall(url string, client *http.Client) ([]byte, error) {
 	resp, err := client.Get(url)
-	if err != nil{
-		return "", errors.New("can not perform get err:" + err.Error())
+	if err != nil {
+		return nil, errors.New("can not perform get err:" + err.Error())
 	}
 	defer resp.Body.Close()
 
-	if !isRespValid(resp.StatusCode){
-		return "",errors.New("got a non standard response code:" + resp.Status)
+	if !isRespValid(resp.StatusCode) {
+		return nil, errors.New("got a non standard response code:" + resp.Status)
 	}
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return "", errors.New("could not read body:" + err.Error())
+		return nil, errors.New("could not read body:" + err.Error())
 	}
 
-	var responseObject Response
-    err = json.Unmarshal(body, &responseObject)
-	if err != nil {
-		return  "", err
-	}
-
-	// split this later
-	if responseObject.Status == "fail"{
-		return "", errors.New("IP location could not be determined because:" + responseObject.Message )
-	}
-
-	return  responseObject.RegionName, nil
-
+	return body, nil
 }
 
-func GetRegionFromContext(c *gin.Context, baseURL string) (string, error){
-	ip := c.RemoteIP()
-	return getRegion(construct_url(baseURL, ip), http.DefaultClient)
+func validate_body(body []byte) (*Response, error) {
+	var responseObject *Response = &Response{}
+	err := json.Unmarshal(body, responseObject)
+	if err != nil {
+		return responseObject, err
+	}
+	if responseObject.Status == "fail" {
+		return responseObject, errors.New("IP location could not be determined because:" + responseObject.Message)
+	}
+
+	return responseObject, nil
+}
+
+func GetRegionFromContext(c *gin.Context, baseURL string) (string, error) {
+	ip := c.ClientIP()
+	body, err := makeHttpCall(construct_url(baseURL, ip), http.DefaultClient)
+	if err != nil {
+		return "", err
+	}
+	res, err := validate_body(body)
+	if err != nil {
+		return "", err
+	}
+	return res.RegionName, nil
 
 }
